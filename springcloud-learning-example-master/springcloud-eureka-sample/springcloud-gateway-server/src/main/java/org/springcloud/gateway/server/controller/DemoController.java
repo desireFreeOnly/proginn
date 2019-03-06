514ad7c3e2958000debe7bc07600424a46160900
dfwl <dfwl@DESKTOP-B0B05CG>
2019-03-06 10:53:04 +0800

    微服务网关,Zuul的主要功能是路由转发和过滤器

---
+++ springcloud-learning-example-master/springcloud-eureka-sample/springcloud-gateway-server/src/main/java/org/springcloud/gateway/server/controller/DemoController.java
@@ -0,0 +1,14 @@
+package org.springcloud.gateway.server.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+	@GetMapping("/hello")
+    public String hello(String name) {
+        return "hi," + name + ",this is zuul api! ";
+    }
+}
