37f320eaa7ffbb6802f89a8e1f651387da937918
dfwl <dfwl@DESKTOP-B0B05CG>
2019-03-08 16:33:57 +0800

    增加ribbon测试方法

--- springcloud-learning-example-master/springcloud-eureka-sample/springcloud-eureka-client-customer/src/main/java/org/spring/springcloud/web/CustomerController.java
+++ springcloud-learning-example-master/springcloud-eureka-sample/springcloud-eureka-client-customer/src/main/java/org/spring/springcloud/web/CustomerController.java
@@ -1,8 +1,13 @@
 package org.spring.springcloud.web;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +25,9 @@ public class CustomerController {
 
     @Autowired
     private RestTemplate restTemplate; // HTTP 访问操作类
+    
+    @Autowired
+    LoadBalancerClient loadBalancerClient;
 
     @RequestMapping("/customer")
     public String customer() {
@@ -33,4 +41,14 @@ public class CustomerController {
     public String hello(String name) {
         return "hi," + name + ",this is springcloud-euraka-client-customer ";
     }
+    
+    @GetMapping("/hello_ribbon") 
+    public String hello_ribbon(String name) {
+        ServiceInstance serviceInstance = loadBalancerClient.choose("provider-service");
+        String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/hello?name=" + name;
+        System.out.println("url地址为："+url);
+        Map<String, Object> uriVariables = new HashMap<String, Object>();
+        uriVariables.put("name",name);
+        return restTemplate.getForObject("http://provider-service/hello?name={name}", String.class,uriVariables);
+    } 
 }
